d2efb2a7e04efab20539d18e86315d7787a9346d
MohamedBenAhmed55 <73948600+MohamedBenAhmed55@users.noreply.github.com>
2022-08-05 17:09:08 +0100

    WorldScramble first level template

---
+++ app/src/main/java/com/example/mentalgym/WSSlvl1.java
@@ -0,0 +1,41 @@
+package com.example.mentalgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class WSSlvl1 extends AppCompatActivity {
+
+    TextView textScreen, textQuestion, textTitle, textBtn;
+    ImageView bigboss;
+    Animation smalltobig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wsslvl1);
+
+        smalltobig = AnimationUtils.loadAnimation(this, R.anim.smalltobig);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOneRegular.ttf");
+
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        textScreen = (TextView) findViewById(R.id.textScreen);
+        textTitle = (TextView) findViewById(R.id.textTitle);
+        textBtn = (TextView) findViewById(R.id.textBtn);
+
+        bigboss = (ImageView) findViewById(R.id.bigboss);
+        bigboss.startAnimation(smalltobig);
+
+
+        textQuestion.setTypeface(typeface);
+        textScreen.setTypeface(typeface);
+        textTitle.setTypeface(typeface);
+        textBtn.setTypeface(typeface);
+    }
+}
