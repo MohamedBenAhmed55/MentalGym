c71649ffa0915c608f94da3a988dbf5dc77a2501
MohamedBenAhmed55 <73948600+MohamedBenAhmed55@users.noreply.github.com>
2022-08-05 14:53:59 +0100

    levels

---
+++ app/src/main/java/com/example/mentalgym/WorScrambleLevels.java
@@ -0,0 +1,30 @@
+package com.example.mentalgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WorScrambleLevels extends AppCompatActivity implements View.OnClickListener {
+
+    private Button lvl1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wor_scramble_levels);
+
+        lvl1 = findViewById(R.id.lvl1);
+        lvl1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.lvl1){
+            Intent i = new Intent(this,MainActivity.class);
+            startActivity(i);
+        }
+    }
+}
--- app/src/main/java/com/example/mentalgym/WordScramble.java
+++ app/src/main/java/com/example/mentalgym/WordScramble.java
@@ -2,13 +2,32 @@ package com.example.mentalgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WordScramble extends AppCompatActivity {
+public class WordScramble extends AppCompatActivity implements View.OnClickListener {
+
+    private Button Easy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_scramble);
+
+        Easy = findViewById(R.id.Easy);
+        Easy.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.Easy) {
+            Intent i = new Intent(this, WorScrambleLevels.class);
+            startActivity(i);
+        }
     }
 }
