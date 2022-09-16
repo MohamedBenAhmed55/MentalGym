d7c04f63b4bb4c9ba5d4b34602301f4bf99534ca
Chaima-Messadi <messadi.chaimaa@gmail.com>
2022-09-16 08:56:12 +0100

    Sounds

--- app/src/main/java/com/example/mentalgym/MCWin.java
+++ app/src/main/java/com/example/mentalgym/MCWin.java
@@ -34,7 +34,7 @@ public class MCWin extends AppCompatActivity implements  View.OnClickListener{
         btnNext.setOnClickListener(this);
 
         // back to home
-        Intent i = new Intent(this, MemoryCardsLevels.class);
+        Intent i = new Intent(this, MainActivity.class);
         txtBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
